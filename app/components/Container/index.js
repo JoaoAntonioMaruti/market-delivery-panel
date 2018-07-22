691e83758e4e056d2fadaec40cdeac0da607c4ab
João Antonio <joaoantoniomaruti@gmail.com>
2018-07-22 16:44:59 -0300

    Create Container component

---
+++ app/components/Container/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display:flex;
+  padding:10px 0;
+  height:100vh;
+  flex-direction:column;
+`;
+
+export { Container };
--- app/global-styles.js
+++ app/global-styles.js
@@ -11,20 +11,4 @@ injectGlobal`
   body {
     font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
   }
-
-  body.fontLoaded {
-    font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-  }
-
-  #app {
-    background-color: #fafafa;
-    min-height: 100%;
-    min-width: 100%;
-  }
-
-  p,
-  label {
-    font-family: Georgia, Times, 'Times New Roman', serif;
-    line-height: 1.5em;
-  }
 `;
