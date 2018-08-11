2d7da9e510904eca2410bb94cce7c678010523eb
João Antonio <joaoantoniomaruti@gmail.com>
2018-08-11 14:27:42 -0300

    add CRUD containers

---
+++ app/containers/Marca/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
+
+import injectReducer from 'utils/injectReducer';
+import injectSaga from 'utils/injectSaga';
+import reducer from './reducer';
+import saga from './saga';
+
+/* eslint-disable react/prefer-stateless-function */
+export class Marca extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+Marca.propTypes = {};
+
+export function mapDispatchToProps(dispatch) {
+  return {};
+}
+
+const mapStateToProps = createStructuredSelector({});
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+const withReducer = injectReducer({ key: 'marca', reducer });
+const withSaga = injectSaga({ key: 'marca', saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(Marca);
---
+++ app/containers/Tag/reducer.js
@@ -0,0 +1,15 @@
+import { fromJS } from 'immutable';
+
+export const initialState = fromJS({});
+export const EXAMPLE_ACTION = 'boilerplate/Component/EXAMPLE_ACTION';
+
+function tagReducer(state = initialState, action) {
+  switch (action.type) {
+    case EXAMPLE_ACTION:
+      return state.set('state_nmae', 'state_value');
+    default:
+      return state;
+  }
+}
+
+export default tagReducer;
