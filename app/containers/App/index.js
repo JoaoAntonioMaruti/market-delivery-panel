7d6a688ddd834296699ba7c7d3ef175e016f52fa
João Antonio <joaoantoniomaruti@gmail.com>
2018-08-11 14:32:47 -0300

    Create CRDE routes

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -5,6 +5,10 @@ import { Switch, Route } from 'react-router-dom';
 
 import LoginPage from 'containers/Login';
 import DashboradPage from 'containers/Dashboard';
+import MarcaPage from 'containers/Marca';
+import ProdutoPage from 'containers/Produto';
+import PropriedadePage from 'containers/Propriedade';
+import TagPage from 'containers/Tag';
 import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
@@ -25,6 +29,10 @@ export default function App() {
         <Route exact path="/" component={LoginPage} />
         <Route exact path="/dashboard" component={DashboradPage} />
         <Route path="/features" component={FeaturePage} />
+        <Route path="/marca" component={MarcaPage} />
+        <Route path="/tag" component={TagPage} />
+        <Route path="/propriedade" component={PropriedadePage} />
+        <Route path="/produto" component={ProdutoPage} />
         <Route path="" component={NotFoundPage} />
       </Switch>
     </AppWrapper>
