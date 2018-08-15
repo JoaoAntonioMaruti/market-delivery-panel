fd7c0676af2b705e9397b6a50b1bc85d7b4755a7
João Antonio <joaoantoniomaruti@gmail.com>
2018-08-14 22:51:59 -0300

    Register user container

--- app/containers/Usuario/index.js
+++ app/containers/Usuario/index.js
@@ -10,8 +10,10 @@ import saga from './saga';
 
 import { Container, CadastroBox, ButtonContainer } from './styles';
 import Input from 'components/Input';
-import Checkbox from 'components/Checkbox';
+import { createAccount } from './actions';
 import Button from 'components/Button';
+import Errors from 'components/FormError';
+import extractMessage from './../../utils/serverErrors';
 
 /* eslint-disable react/prefer-stateless-function */
 export class Usuario extends React.PureComponent {
@@ -19,6 +21,7 @@ export class Usuario extends React.PureComponent {
     super(props);
     this.state = {
       status: 1,
+      errors: [],
     };
   }
 
@@ -28,10 +31,37 @@ export class Usuario extends React.PureComponent {
   }
 
   submit() {
-    console.log(this.state);
+    const { nome, login, senha, status } = this.state;
+    const user = { nome, login, senha, status };
+
+    this.setState({
+      nomeError: !login ? 'Entre com o Nome' : '',
+      loginError: !login ? 'Entre com o usuário' : '',
+      senhaError: !senha ? 'Entre com a senha' : '',
+    });
+
+    if (nome && login && senha) {
+      createAccount(user).then(response => {
+        if (response.data) {
+          const { token } = response.data;
+
+          localStorage.setItem('token', token);
+          return (window.location = '/');
+        }
+        const errors = extractMessage(response.message);
+        return this.setState({ errors });
+      });
+    }
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
   }
 
   render() {
+    const { nomeError, loginError, senhaError, errors } = this.state;
+
     return (
       <Container>
         <CadastroBox>
@@ -39,24 +69,28 @@ export class Usuario extends React.PureComponent {
             placeholder={'Nome'}
             onChange={event => this.handleChange(event)}
             name={'nome'}
-            label={'Nome'}
+            label={'Nome *'}
+            onChange={value => this.handleChange(value)}
             type={'text'}
+            error={nomeError}
           />
           <Input
             placeholder={'Nome de usuário'}
             name={'login'}
             onChange={event => this.handleChange(event)}
-            label={'Nome de usuário'}
+            label={'Nome de usuário *'}
             type={'text'}
+            error={loginError}
           />
           <Input
             placeholder={'Senha'}
             name={'senha'}
             onChange={event => this.handleChange(event)}
-            label={'Senha'}
+            label={'Senha *'}
             type={'password'}
+            error={senhaError}
           />
-
+          <Errors messages={errors} />
           <ButtonContainer>
             <Button title={'Cadastrar'} onClick={() => this.submit()} />
           </ButtonContainer>
